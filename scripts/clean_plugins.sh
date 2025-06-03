5c9ea7c9839e7f0aac1e8f566a1b9dcd405e8609
^x3ro <git@x3ro.dev>
2025-06-03 11:27:30 +0200

    🐛 FIX: Uninstall plugins by their exact name

--- scripts/clean_plugins.sh
+++ scripts/clean_plugins.sh
@@ -12,15 +12,33 @@ else # shell output functions
 	source "$HELPERS_DIR/shell_echo_functions.sh"
 fi
 
+_get_installed_plugin_names() {
+	local plugins plugin plugin_names
+	plugins="$(tpm_plugins_list_helper)"
+
+	plugin_names=()
+	for plugin in $plugins; do
+		# Remove everything after `#`
+		plugin="${plugin%%#*}"
+		# Remove username and slash
+		plugin="${plugin##*/}"
+
+		plugin_names+=( $plugin )
+	done
+
+	echo "${plugin_names[*]}"
+}
+
 clean_plugins() {
 	local plugins plugin plugin_directory
-	plugins="$(tpm_plugins_list_helper)"
+	plugins="$(_get_installed_plugin_names)"
 
 	for plugin_directory in "$(tpm_path)"/*; do
 		[ -d "${plugin_directory}" ] || continue
 		plugin="$(plugin_name_helper "${plugin_directory}")"
-		case "${plugins}" in
-			*"${plugin}"*) : ;;
+		# Add spaces around plugin list to allow matching the full name
+		case " ${plugins} " in
+			*" ${plugin} "*) : ;;
 			*)
 			[ "${plugin}" = "tpm-async" ] && continue
 			echo_ok "Removing \"$plugin\""
